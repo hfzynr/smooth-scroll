74e0841779ae94e97e14137f377da56b301d6f6a
hafizyanuar <hafizyanuaribrahim@next-ti.co.id>
2021-06-18 13:52:52 +0700

    done styling Navbar and Sidebar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,4 +1,14 @@
 import React from 'react'
+import {
+    SidebarContainer,
+    Icon,
+    CloseIcon,
+    SidebarWrapper,
+    SidebarMenu,
+    SidebarLink,
+    SideBtnWrap,
+    SidebarRoute
+} from './SidebarElements'
 
 const Sidebar = () => {
     return (
@@ -6,6 +16,25 @@ const Sidebar = () => {
             <Icon>
                 <CloseIcon />
             </Icon>
+            <SidebarWrapper>
+                <SidebarMenu>
+                    <SidebarLink to="about">
+                        About
+                    </SidebarLink>
+                    <SidebarLink to="discover">
+                        Discover
+                    </SidebarLink>
+                    <SidebarLink to="services">
+                        Services
+                    </SidebarLink>
+                    <SidebarLink to="signup">
+                        Sign Up
+                    </SidebarLink>
+                </SidebarMenu>
+                <SideBtnWrap>
+                    <SidebarRoute to="/signin">Sign In</SidebarRoute>
+                </SideBtnWrap>
+            </SidebarWrapper>
         </SidebarContainer>
     )
 }
